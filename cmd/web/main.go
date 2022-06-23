13445c8828c873cb0e34e24bca6e499bc814524d
storyteller <storyteller0205@gmail.com>
2022-06-23 16:41:33 +0600

    12

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/ascii-art", PostAsciiArt)
-
+	port := ":" + os.Getenv("PORT")
 	log.Println("Server is listening on http://127.0.0.1:8181...")
-	http.ListenAndServe(":8181", nil)
+	http.ListenAndServe(port, nil)
 }
