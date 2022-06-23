9ed4d1036a398d8c8f5f5221d880656652a3f984
storyteller <storyteller0205@gmail.com>
2022-06-23 16:38:39 +0600

    1

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/ascii-art", PostAsciiArt)
 
-	log.Println("Server is listening on http://localhost:8181...")
+	log.Println("Server is listening on http://127.0.0.1:8181...")
 	http.ListenAndServe(":8181", nil)
 }
